904d7cbf978a7dc0ce6023f73b849f538c4194a8
2902150237-dotcom <2902150237@qq.com>
2026-07-29 02:45:08 +0800

    feat(platform): bootstrap Spring Boot service

---
+++ openpulse-platform/src/main/java/io/github/lihanc940/openpulse/OpenPulseApplication.java
@@ -0,0 +1,12 @@
+package io.github.lihanc940.openpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenPulseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenPulseApplication.class, args);
+    }
+}
